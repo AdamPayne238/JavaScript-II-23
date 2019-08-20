05bcf72b8500c244e6baad502c355cd1f37e6ba2
Adam Payne <ajpayne238@gmail.com>
2019-08-20 18:06:58 -0400

    test

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -4,66 +4,95 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 /* 
 
-  // GIVEN THIS PROBLEM:
-
+  //Given this problem: 
+  
   function firstItem(arr, cb) {
     // firstItem passes the first item of the given array to the callback function.
   }
 
-  // SOLUTION:
+  // Potential Solution:
 
+  // Higher order function using "cb" as the call back
   function firstItem(arr, cb) {
     return cb(arr[0]);
   }
 
-  // NOTES ON THE SOLUTION:
+  // Function invocation 
+  firstItem(items, function(first) {
+    console.log(first)
+  });
 
-  // firstItem is a higher order function.
-  // It expects a callback (referred to as `cb`) as its second argument.
-  // To test our solution, we will use the given `items` array and a variety of callbacks.
-  // Note how callbacks can be declared separately, or inlined.
-
-  // TEST 1 (inlined callback):
-
-  firstItem(items, item => console.log(`I love my ${item}!`));
-  // "I love my Pencil!"
-
-  // TEST 2 (declaring callback before hand):
-
-  function logExorbitantPrice(article) {
-    console.log(`this ${article} is worth a million dollars!`);
-  };
-
-  firstItem(items, logExorbitantPrice);
-  // "this Pencil is worth a million dollars!"
 */
+// function add(num1, num2){
+//   return num1 + num2;
+// }
 
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
+getLength(items, (length)=>{
+  console.log(length);
+});
+//done
+
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[items.length -1])
 }
+last(items, (last)=>{
+  console.log(last)
+});
+//done
 
-function sumNums(x, y, cb) {
-  // sumNums adds two numbers (x, y) and passes the result to the callback.
+
+function add(num1, num2){
+  return num1 + num2;
 }
+function multiply(num1, num2){
+  return num1 * num2;
+}
+function sumNums(x, y, cb) {
+// sumNums adds two numbers (x, y) and passes the result to the callback.
+  console.log(cb)
+  return cb(x, y);
+}
+console.log(sumNums(5, 5, add))
+
 
 function multiplyNums(x, y, cb) {
-  // multiplyNums multiplies two numbers and passes the result to the callback.
+// multiplyNums multiplies two numbers and passes the result to the callback.
+console.log(cb)
+return cb(x, y);
 }
+console.log(multiplyNums(4, 11, multiply))
+
+
+//Break
+
+
 
 function contains(item, list, cb) {
-  // contains checks if an item is present inside of the given array/list.
-  // Pass true to the callback if it is, otherwise pass false.
-}
+//   // contains checks if an item is present inside of the given array/list.
+//   // Pass true to the callback if it is, otherwise pass false.
+  for(let i = 0; i < list.length; i++) {
+    if(list[i] === item){ 
+    return true;
+    }else{ return false;
+  }
+}	
+console.log(contains(cb));
+
+
+//Break
+
 
 /* STRETCH PROBLEM */
 
-function removeDuplicates(array, cb) {
+// function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
-}
+// }
